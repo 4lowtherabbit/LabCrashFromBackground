2c4e178929eaab4e14b1ddd01028e2cb90b211f0
juzhu <juzhu@microsoft.com>
2019-07-08 15:25:18 +0800

    Add project files.

---
+++ LabCrashFromBackground/Controllers/ReproController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.Mvc;
+
+namespace LabCrashFromBackground.Controllers
+{
+    public class ReproController : Controller
+    {
+        // GET: Repro
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Crash()
+        {
+            HostingEnvironment.QueueBackgroundWorkItem(Action => {
+                Thread.Sleep(10000);
+                throw new ApplicationException("Crashing...");
+            });
+            return "Process will crash in 10 seconds";
+        }
+    }
+}
